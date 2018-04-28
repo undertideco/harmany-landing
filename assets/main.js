8b72cf4df05fe8f1167bea9813fa97f3ee962018
Jurvis Tan <jurvis.tan@me.com>
2018-04-28 11:40:41 +0800

    do not redirect if fields are empty

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,14 @@
 (function() {
   document.getElementById('beta-btn-1').onclick = function() {
     var emailFieldValue = document.getElementById('email-field-1').value
+    if (emailFieldValue === "") { return }
     window.open('https://beta.harmany.me/' + '?email=' + encodeURI(emailFieldValue),
     '_blank');
   }
 
   document.getElementById('beta-btn-2').onclick = function() {
     var emailFieldValue = document.getElementById('email-field-2').value
+    if (emailFieldValue === "") { return }
     window.open('https://beta.harmany.me/' + '?email=' + encodeURI(emailFieldValue),
     '_blank');
   }
